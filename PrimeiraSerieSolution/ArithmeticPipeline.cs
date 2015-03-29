912b89394675f418f19cb0cd219e12333427f025
Pedro Gabriel <pedro.13a@gmail.com>
2015-03-29 22:39:50 +0100

    solution

---
+++ PrimeiraSerieSolution/ArithmeticPipeline.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraSerieSolution
+{
+    class ArithmeticPipeline
+    {
+        /* ... */
+        public void Add(IArrayOperation op) { /* ... */ }
+        public void ExecuteAll(double[] startArray)
+        { /* ... */ }
+    }
+}
