d7ab09ebd7c513a5c772eccb0de0f3c5f39626e4
JoaoMDSCaetano <joao_mds_caetano@live.com.pt>
2015-04-10 22:00:45 +0100

    Implemented MethodExecuter Class (execute doesnt work)
    Updated loaders.loadmethods()(not fullydone)

---
+++ pipemath/ArithmeticPipeline/MethodExecuter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace PipeMath
+{
+    class MethodExecuter : IArrayOperation
+    {
+        private MethodInfo info; 
+        private Type type;
+
+        public MethodExecuter(MethodInfo info, Type type)
+        {
+            this.info=info;
+            this.type=type;
+        }
+
+        public void Execute(double[] values)
+        {
+          //  info.Invoke(Activator.CreateInstance(type),v);
+        }
+
+    }
+}
