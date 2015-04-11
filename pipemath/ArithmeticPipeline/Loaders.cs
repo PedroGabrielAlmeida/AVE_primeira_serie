42a8b3b4bb8837aef93cb5766d5c6c233a235aa5
Pedro Gabriel <pedro.13a@gmail.com>
2015-04-11 19:16:23 +0100

    Testes executados com sucesso.

--- pipemath/ArithmeticPipeline/Loaders.cs
+++ pipemath/ArithmeticPipeline/Loaders.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using PipeMath;
 
 namespace PipeMath
 {
@@ -48,16 +49,45 @@ namespace PipeMath
         {
             _allOps = new List<IArrayOperation>();
             Assembly assembly = Assembly.Load(file);
-            
-            Type[] types = assembly.GetTypes();
-            foreach (Type type in types)
+            LoadMethods(assembly.GetExportedTypes());
+
+        }
+
+        private void LoadMethods(Type[] exportedTypes)
+        {
+            //Execute's info
+            MethodInfo executeMethodInfo = typeof (IArrayOperation).GetMethod("Execute");
+            ParameterInfo[] executeParameters = executeMethodInfo.GetParameters();
+            foreach (Type type in exportedTypes)
             {
-                MethodInfo[] methodInfos = type.GetMethods(BindingFlags.Public | BindingFlags.Static);
+                MethodInfo[] methodInfos = type.GetMethods();
                 foreach (var methodInfo in methodInfos)
                 {
-                    //...
+                    if (IsMethodInfoValid(executeMethodInfo, methodInfo))
+                    {
+                        _allOps.Add(methodInfo.IsStatic
+                            ? new MethodExecuter(methodInfo)
+                            : new MethodExecuter(methodInfo, type));
+                    }
                 }
             }
+        }
+
+        private bool IsMethodInfoValid(MethodInfo executeInfo, MethodInfo methodInfo)
+        {
+            //tipo de retorno
+            if (executeInfo.ReturnType != methodInfo.ReturnType) return false;
+ 
+            //tipos de parametro
+            ParameterInfo[] methodsParams = methodInfo.GetParameters(),
+                            executesParams = executeInfo.GetParameters();
+            if (methodsParams.Length != executesParams.Length) return false;
+            for (int i = 0; i < methodsParams.Length; i++)
+            {
+                //reference equality
+                if (!(methodsParams[i].ParameterType == executesParams[i].ParameterType)) return false;
+            }
+            return true;
         }
 
         public List<IArrayOperation> LoadOperations()
--- pipemath/ArithmeticPipeline/MethodExecuter.cs
+++ pipemath/ArithmeticPipeline/MethodExecuter.cs
@@ -9,18 +9,24 @@ namespace PipeMath
 {
     class MethodExecuter : IArrayOperation
     {
-        private MethodInfo info; 
-        private Type type;
+        private readonly MethodInfo _methodInfo; 
+        private readonly Type _methodType;
 
-        public MethodExecuter(MethodInfo info, Type type)
+        public MethodExecuter(MethodInfo methodMethodInfo)
         {
-            this.info=info;
-            this.type=type;
+            _methodInfo = methodMethodInfo;
+            _methodType = null;
+        }
+
+        public MethodExecuter(MethodInfo methodMethodInfo, Type methodType)
+        {
+            _methodInfo=methodMethodInfo;
+            _methodType=methodType;
         }
 
         public void Execute(double[] values)
         {
-          //  info.Invoke(Activator.CreateInstance(type),v);
+            _methodInfo.Invoke(Activator.CreateInstance(_methodType), new object[]{values});
         }
 
     }
